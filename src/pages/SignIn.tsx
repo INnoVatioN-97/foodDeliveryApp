a4e39ddeace8f16375726eb9d4c577e03c22333e
YOUNGIL KO <koyoungil97@gmail.com>
2022-07-04 20:56:07 +0900

    로그인 화면 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
-import React, {useCallback, useState} from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useCallback, useRef, useState} from 'react';
 import {
   Alert,
   Pressable,
@@ -7,21 +8,36 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import {RootStackParamList} from '../../App';
 
-function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
 
-  const onChangeEmail = useCallback(text => {
-    setEmail(text);
+function SignIn({navigation}: SignInScreenProps): JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const emailRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const onChangeEmail = useCallback((text: string): void => {
+    setEmail(text.trim());
   }, []);
-  const onChangePassword = useCallback(text => {
-    setPassword(text);
+  const onChangePassword = useCallback((text: string) => {
+    setPassword(text.trim());
   }, []);
 
   const onSubmit = useCallback(() => {
-    Alert.alert('응애!');
-  }, []);
+    if (!email || !email.trim())
+      return Alert.alert('알림', '이메일을 입력해 주세요.');
+    if (!password || !password.trim())
+      return Alert.alert('알림', '비밀번호를 입력해 주세요.');
+
+    Alert.alert('알림', '로그인 되었습니다.');
+  }, [email, password]);
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
 
   const canGoNext = email && password;
 
@@ -31,16 +47,36 @@ function SignIn() {
         <Text style={styles.label}>이메일</Text>
         <TextInput
           style={styles.textInput}
+          value={email}
           onChangeText={onChangeEmail}
           placeholder="이메일을 입력하세요."
+          importantForAutofill="yes"
+          autoComplete="email"
+          textContentType="emailAddress"
+          returnKeyType="next"
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+          ref={emailRef}
+          blurOnSubmit={false}
+          clearButtonMode="while-editing"
+          keyboardType="email-address"
         />
       </View>
       <View>
         <Text style={styles.label}>비밀번호</Text>
         <TextInput
           style={styles.textInput}
+          value={password}
           onChangeText={onChangePassword}
           placeholder="비밀번호를 입력하세요."
+          secureTextEntry
+          importantForAutofill="yes"
+          autoComplete="password"
+          textContentType="password"
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
+          clearButtonMode="while-editing"
         />
       </View>
 
@@ -55,7 +91,7 @@ function SignIn() {
           disabled={!canGoNext}>
           <Text style={styles.loginButtonText}>로그인</Text>
         </Pressable>
-        <Pressable>
+        <Pressable onPress={toSignUp}>
           <Text>회원가입</Text>
         </Pressable>
       </View>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,12 +1,152 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useCallback, useRef, useState} from 'react';
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {RootStackParamList} from '../../App';
 
-function SignUp() {
+type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
+
+function SignUp({navigation}: SignUpScreenProps): JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const emailRef = useRef<TextInput | null>(null);
+  const nameRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const onChangeEmail = useCallback((text: string): void => {
+    setEmail(text.trim());
+  }, []);
+  const onChangePassword = useCallback((text: string) => {
+    setPassword(text.trim());
+  }, []);
+  const onChangeName = useCallback((text: string) => {
+    setName(text.trim());
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (!email || !email.trim()) {
+      return Alert.alert('알림', '이메일을 입력해 주세요.');
+    }
+    if (!name || !name.trim()) {
+      return Alert.alert('알림', '이름을 입력해 주세요.');
+    }
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해 주세요.');
+    }
+    if (
+      !/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(
+        email,
+      )
+    ) {
+      return Alert.alert('알림', '올바른 이메일 주소가 아닙니다.');
+    }
+    if (!/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@^!%*#?&]).{8,50}$/.test(password)) {
+      return Alert.alert(
+        '알림',
+        '비밀번호는 영문,숫자,특수문자($@^!%*#?&)를 모두 포함하여 8자 이상 입력해야합니다.',
+      );
+    }
+    console.log(email, name, password);
+    Alert.alert('알림', '회원가입 되었습니다.');
+  }, [email, name, password]);
+
+  const canGoNext = email && password && name;
+
   return (
-    <View>
-      <Text>로그인</Text>
-    </View>
+    <KeyboardAvoidingView behavior="position">
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeEmail}
+          placeholder="이메일을 입력해주세요"
+          placeholderTextColor="#666"
+          textContentType="emailAddress"
+          value={email}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          keyboardType="email-address"
+          ref={emailRef}
+          onSubmitEditing={() => nameRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이름</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이름을 입력해주세요."
+          placeholderTextColor="#666"
+          onChangeText={onChangeName}
+          value={name}
+          textContentType="name"
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={nameRef}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
+          placeholderTextColor="#666"
+          onChangeText={onChangePassword}
+          value={password}
+          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
+          textContentType="password"
+          secureTextEntry
+          returnKeyType="send"
+          clearButtonMode="while-editing"
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={
+            canGoNext
+              ? StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+              : styles.loginButton
+          }
+          disabled={!canGoNext}
+          onPress={onSubmit}>
+          <Text style={styles.loginButtonText}>회원가입</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  inputWrapper: {padding: 20},
+  textInput: {padding: 5, borderBottomWidth: StyleSheet.hairlineWidth},
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonZone: {alignItems: 'center'},
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  loginButtonActive: {backgroundColor: 'blue'},
+  loginButtonText: {color: 'white', fontSize: 16},
+});
 
 export default SignUp;
